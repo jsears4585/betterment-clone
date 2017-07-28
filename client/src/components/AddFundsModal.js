1deb12f67a009eb778b134c54855f6d77c000304
Jason Sears <jsears4585@gmail.com>
2017-07-28 15:45:24 -0400

    Various CSS, refactored accordian

--- client/src/components/AddFundsModal.js
+++ client/src/components/AddFundsModal.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const AddFundsModal = props => {
   return (
-    <div>
-      <h2 className="inlineBlockHeader">Add Funds</h2><span className="plusIcon"><i className="big plus square outline icon"></i></span>
+    <div className="addMoney">
+      <h3 className="inlineBlockHeader">Add Funds +</h3>
     </div>
   )
 }
--- client/src/components/DetailsAccordion.js
+++ client/src/components/DetailsAccordion.js
@@ -1,17 +1,51 @@
 import React from 'react'
-import { Accordion } from 'semantic-ui-react'
+import { Accordion, Icon } from 'semantic-ui-react'
 
 const DetailsAccordion = props => {
 
-  const panels = [
-    {title: 'VOO', content: 'Current price: $226.55'},
-    {title: 'BND', content: 'Current price: $81.925'},
-    {title: 'IGOV', content: 'Current price: $98.11'},
-    {title: 'VXUS', content: 'Current price: $53.52'}
-  ]
-
   return (
-    <Accordion panels={panels} styled />
+    <Accordion styled>
+      <Accordion.Title onClick={()=>(console.log('clicked VOO'))}>
+        <Icon name='dropdown' />
+        VOO
+      </Accordion.Title>
+      <Accordion.Content>
+        Price: <br />
+        Mkt Cap: <br />
+        Expenses: <br />
+        Category: 
+      </Accordion.Content>
+      <Accordion.Title onClick={()=>(console.log('clicked BND'))}>
+        <Icon name='dropdown' />
+        BND
+      </Accordion.Title>
+      <Accordion.Content>
+        Price: <br />
+        Mkt Cap: <br />
+        Expenses: <br />
+        Category:
+      </Accordion.Content>
+      <Accordion.Title onClick={()=>(console.log('clicked IGOV'))}>
+        <Icon name='dropdown' />
+        IGOV
+      </Accordion.Title>
+      <Accordion.Content>
+        Price: <br />
+        Mkt Cap: <br />
+        Expenses: <br />
+        Category:
+      </Accordion.Content>
+      <Accordion.Title onClick={()=>(console.log('clicked VXUS'))}>
+        <Icon name='dropdown' />
+        VXUS
+      </Accordion.Title>
+      <Accordion.Content>
+        Price: <br />
+        Mkt Cap: <br />
+        Expenses: <br />
+        Category:
+      </Accordion.Content>
+    </Accordion>
   )
 }
 
