148fadfd6bd46734122a11498390c296d539d7ca
Brett Schules <brettschules@gmail.com>
2017-07-31 18:48:53 -0400

    Can add new bank

--- client/src/components/AddFundsModel.js
+++ client/src/components/AddFundsModel.js
@@ -15,25 +15,67 @@ export default class AddFundsModal extends React.Component{
       text: "TD Bank",
       value: 'TD Bank'
     }
-    ]
-    }
+    ],
+    addBankForm: false,
+    bankInput: ""
+  }
+
+  }
+
+  handleClickAddBank = () => {
+    this.setState({
+      addBankForm: true
+    })
+  }
+
+  handleClickCloseBank = () => {
+    this.setState({
+      addBankForm: false
+    })
+  }
+
+  handleFormInput = (event) => {
+    this.setState({
+      bankInput: event.target.value
+    })
+
+  }
+
+  handleFormSubmit = (event) => {
+    debugger
+    event.preventDefault()
+    this.setState({
+      bankOptions: [...this.state.bankOptions,
+        {text: this.state.bankInput,
+        value: this.state.bankInput}
+      ]
+    })
   }
 
   render(){
+    console.log(this.state.bankInput)
     return(
       <div>
-      <Modal open="true" size="huge" className="add-funds-modal">
+      <Modal open={true} size="huge" className="add-funds-modal">
           <Modal.Header className="funds-headers">Add Funds</Modal.Header>
           <Modal.Content >
           <Modal.Header className="funds-headers">Select your bank account</Modal.Header>
             <div className="banks">
               <Dropdown placeholder='State' search selection options={this.state.bankOptions} />
+              <Button icon="plus circle" id="add-bank" color="blue" onClick={this.handleClickAddBank}></Button>
+              { !this.state.addBankForm ? <div></div> :
+              <form onSubmit={this.handleFormSubmit}>
+                <input type="text" name="bank" id="add-bank-form" onChange={this.handleFormInput}/>
+                <Button type="submit" color="blue"  >Add</Button>
+              </form>
+
+            }
             </div>
         <Modal.Description>
           <Header className="funds-headers">Amount</Header>
-          <Form className="add-funds-form">
-            <Form.Group>
-              <Form.Field id="form-amount-input"  control={Input} placeholder='Amount(USD)' />
+          <Form className="add-funds-form" >
+            <Form.Group >
+              <Form.Field id="form-amount-input" width="16" control={Input} placeholder='Amount(USD)' />
             </Form.Group>
             <Form.Group widths='equal'>
               <Form.Field id="form-cancel-button" control={Button} size="big" primary="true" content='Cancel' onClick={this.props.handleClickExitFundsModel} />
